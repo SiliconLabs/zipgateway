bdea7d357ffffddfc488dcf3081eb4c9cab01887
Anh Anh Nguyen Duc [C] <annguyen5@silabs.com>
2025-02-04 14:33:51 +0700

    Porting on Debian 10

--- helper.mk
+++ helper.mk
@@ -86,6 +86,15 @@ setup/debian/stretch: /etc/apt/sources.list /etc/os-release
 	${sudo} apt-get update
 	${sudo} apt-get install -y ${packages}
 
+setup/debian/buster: /etc/apt/sources.list /etc/os-release
+	grep 'VERSION="10 (buster)"' /etc/os-release
+#	${sudo} sed -e 's|\(http://\)\(.*\)\(.debian.org\)|\1archive\3|g' -i $<
+#	${sudo} sed -e 's|stretch-updates|stretch-proposed-updates|g' -i $<
+#	echo "deb http://archive.debian.org/debian stretch-backports main contrib non-free" \
+| ${sudo} tee "$<.d/backports.list"
+	${sudo} apt-get update
+	${sudo} apt-get install -y ${packages}
+
 setup/debian: setup/debian/stretch
 	echo "# $@"
 
